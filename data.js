0e142ae77d332b70381c84209284817a2be31188
NeupOrng <neuporng18@kit.edu.kh>
2020-02-02 17:15:26 +0700

    first building

---
+++ data.js
@@ -0,0 +1,25 @@
+
+var home = new Vue({
+  el: '#homepage',
+  data:{
+    title: 'Hello World',
+    content: 'Welcome to this world',
+    backgroundVideo:'background.mp4'
+  }
+})
+
+var information = new Vue({
+  el: '#information-section',
+  data:{
+    titleSection:'information',
+    title1:'world1',
+    content1:'this is the world that we are living in. is it beautifull?',
+    image1:'world1.png',
+    title2:'world2',
+    content2:'Ahh let see the world2 together. this is the world we want everyday',
+    image2:'world2.png',
+    title3:'world3',
+    content3:'hey look this world look like the world1 that we have seen',
+    image3:'world1.png'
+  }
+})
