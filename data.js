ee92f3264e8ab1bf57dc9cf1ba75d2cdf8271771
NeupOrng <neuporng18@kit.edu.kh>
2020-02-05 14:32:09 +0700

    new struture

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 
-var home = new Vue({
+const home = new Vue({
   el: '#homepage',
   data:{
     title: 'Hello World',
@@ -8,18 +8,51 @@ var home = new Vue({
   }
 })
 
-var information = new Vue({
+const information = new Vue({
   el: '#information-section',
   data:{
     titleSection:'information',
-    title1:'world1',
-    content1:'this is the world that we are living in. is it beautifull?',
-    image1:'world1.png',
-    title2:'world2',
-    content2:'Ahh let see the world2 together. this is the world we want everyday',
-    image2:'world2.png',
-    title3:'world3',
-    content3:'hey look this world look like the world1 that we have seen',
-    image3:'world1.png'
+    part1: {
+      title:'world1',
+      content:'this is the world that we are living in. is it beautiful?',
+      image:'world1.png'
+    },
+    part2:{
+      title:'world2',
+      content:'Ahh let see the world2 together. this is the world we want everyday',
+      image:'world2.png'
+    },
+    part3:{
+      title:'world3',
+      content:'hey look this world look like the world1 that we have seen',
+      image:'world1.png'
+    }
+
+  }
+})
+
+const comparation = new Vue({
+  el: '#compare-university-section',
+  data:{
+    mainTitle: 'Compare Other University with KIT',
+    part1: {
+      title: "KIT",
+      content: "ladfladfjlas adla fadslf j"
+    },
+    part2: {
+      title: "USA",
+      content: "ladfladfjlas adla fadslf j"
+    },
+    part3: {
+      title: "Korea",
+      content: "ladfladfjlas adla fadslf j"
+    },
+  }
+})
+const footer = new Vue({
+  el: '#footer-section',
+  data: {
+    logo: "logo.png",
+    description_under_logo: "data more data to this section in the app.js"
   }
 })
